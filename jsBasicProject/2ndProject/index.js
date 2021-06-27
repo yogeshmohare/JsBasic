7eb11ae17c7aae4f2591685fe3a0880c4fe13ca3
Yogesh <mohare.yogesh.18et5003@gmail.com>
2021-06-27 18:41:53 +0530

    Add error msg while purchasing button

---
+++ jsBasicProject/2ndProject/index.js
@@ -0,0 +1,7 @@
+let purch_btn = document.getElementById("purchase-btn");
+let error_msg = document.getElementById("error");
+function purchase() {
+    error_msg.textContent ="Something went wrong please try again later";
+    purch_btn.t =error_msg;
+
+}
